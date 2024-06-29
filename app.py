a4421a0b936c4a7e58d9ad816a199eb61056ef0a
= <=>
2024-06-29 11:15:55 +0500

    added doc strings and types anotation

--- app.py
+++ app.py
@@ -1,43 +1,72 @@
+from typing import Any, Optional
+
+
 class ObjList:
-    def __init__(self, data=None):
+    """Класс для представления элемента в двусвязном списке.
+
+    Атрибуты:
+        __data (Any): Данные, хранящиеся в узле.
+        __prev (Optional[ObjList]): Ссылка на предыдущий узел в списке.
+        __next (Optional[ObjList]): Ссылка на следующий узел в списке.
+    """
+
+    def __init__(self, data: Any = None) -> None:
+        """Инициализирует узел с данными, а также предыдущим и следующим узлами, установленными в None."""
         self.__data = data
         self.__prev = None
         self.__next = None
 
     @property
-    def data(self):
+    def data(self) -> Any:
+        """Возвращает данные узла."""
         return self.__data
 
     @data.setter
-    def data(self, value):
+    def data(self, value: Any) -> None:
+        """Устанавливает данные узла."""
         self.__data = value
 
     @property
-    def prev(self):
+    def prev(self) -> Optional['ObjList']:
+        """Возвращает предыдущий узел списка."""
         return self.__prev
 
     @prev.setter
-    def prev(self, node):
+    def prev(self, node: 'ObjList') -> None:
+        """Устанавливает предыдущий узел списка."""
         self.__prev = node
 
     @property
-    def next(self):
+    def next(self) -> Optional['ObjList']:
+        """Возвращает следующий узел списка."""
         return self.__next
 
     @next.setter
-    def next(self, node):
+    def next(self, node: 'ObjList') -> None:
+        """Устанавливает следующий узел списка."""
         self.__next = node
 
 
 class LinkedList:
-    def __init__(self):
+    """Класс для представления двусвязного списка.
+
+    Атрибуты:
+        head (Optional[ObjList]): Начало списка.
+        tail (Optional[ObjList]): Конец списка.
+    """
+
+    def __init__(self) -> None:
+        """Инициализирует пустой двусвязный список."""
         self.head = None
         self.tail = None
 
-    def add_obj(self, data):
+    def add_obj(self, data: Any) -> None:
+        """Добавляет новый объект в конец списка.
 
+        Параметры:
+            data (Any): Данные для добавления в список.
+        """
         new_obj = ObjList(data)
-
         if self.head is None:
             self.head = new_obj
             self.tail = new_obj
@@ -46,7 +75,8 @@ class LinkedList:
             self.tail.next = new_obj
             self.tail = new_obj
 
-    def remove_obj(self):
+    def remove_obj(self) -> None:
+        """Удаляет последний объект из списка."""
         if self.tail is None:
             return
 
@@ -58,7 +88,12 @@ class LinkedList:
             if self.tail:
                 self.tail.next = None
 
-    def get_data(self):
+    def get_data(self) -> list:
+        """Собирает и возвращает список всех данных в элементах списка.
+
+        Возвращает:
+            list: Список данных всех элементов списка.
+        """
         elements = []
         current = self.head
         while current:
